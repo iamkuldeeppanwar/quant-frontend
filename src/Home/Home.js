ea5a8bd2b85c57100adb85f83d1bfa6afca8c552
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2024-03-20 13:03:15 +0530

    remove pagination

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Pagination } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import request from "../api";
 
@@ -28,51 +28,30 @@ const Home = () => {
   };
 
   return (
-    <div className="d-flex flex-column">
-      <Table responsive>
-        <thead>
-          <tr>
-            <th>S.No</th>
-            <th>Name</th>
-            <th>Date Of Birth</th>
-            <th>Email</th>
-            <th>Password</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users?.map((user, index) => {
-            return (
-              <tr key={user._id}>
-                <td>{index + 1}</td>
-                <td>{user.name}</td>
-                <td>{user.date}</td>
-                <td>{user.email}</td>
-                <td>{user.password}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-      <div className="d-flex justify-content-end">
-        <Pagination>
-          <Pagination.First />
-          <Pagination.Prev />
-          <Pagination.Item>{1}</Pagination.Item>
-          <Pagination.Ellipsis />
-
-          <Pagination.Item>{10}</Pagination.Item>
-          <Pagination.Item>{11}</Pagination.Item>
-          <Pagination.Item active>{12}</Pagination.Item>
-          <Pagination.Item>{13}</Pagination.Item>
-          <Pagination.Item disabled>{14}</Pagination.Item>
-
-          <Pagination.Ellipsis />
-          <Pagination.Item>{20}</Pagination.Item>
-          <Pagination.Next />
-          <Pagination.Last />
-        </Pagination>
-      </div>
-    </div>
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>S.No</th>
+          <th>Name</th>
+          <th>Date Of Birth</th>
+          <th>Email</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users?.map((user, index) => {
+          return (
+            <tr key={user._id}>
+              <td>{index + 1}</td>
+              <td>{user.name}</td>
+              <td>{user.date}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
   );
 };
 
